40ee5b54aba49809ee382045c499cd05bf2bde97
Orjiewuru Kingdom <kingdom.orjiewuru@andela.com>
2017-09-05 12:55:07 +0100

    feature: create basic scaffolding for development environment

---
+++ client/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import App from './components/App.jsx';
+import './assets/styles/styles.css';
+
+render(<App />, document.getElementById('app'));
